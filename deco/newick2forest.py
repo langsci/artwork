3bd9d1773ae728092d1c489ed411a0a15d43c4f6
glottotopia <sebastian.nordhoff@glottotopia.de>
2020-12-15 18:03:59 +0100

    compile all trees

--- deco/newick2forest.py
+++ deco/newick2forest.py
@@ -1,36 +1,51 @@
 from newick import read
 import sys
+import json
 
-def sanitized_name(node):
-    parts = node.name[1:-1].split(" [")
+glottonames = json.loads(open('glottonames.json').read())
+ilis = json.loads(open('ilis.json').read())
+codelevels = {}
+for g in glottonames:
+    codelevels[glottonames[g][0]] = f"g{glottonames[g][1]}"
+
+def sanitized_name(n):
+    parts = n.name[1:-1].split(" [")
     name = parts[0]
     code = parts[1][:-1]
     return (name, code)
 
+def index_status(name, code):
+    if name in ilis:
+        if name in glottonames:
+            if code == glottonames[name][0]: #good match
+                return r'\flaggedgood'
+        return r'\flaggedmid' #potential mismatch
+    return ''
 
 def tikz(node, out, level=0):
     outputname, outputcode  = sanitized_name(node)
     indentation = level * ' '
-    out.write("{%s}[\\namecode{%s}{%s}" % (indentation, outputname, outputcode))
+    option = f"\\{codelevels.get(outputcode,'')}, "
+    option +=  index_status(outputname, outputcode)
+    out.write("%s[\\namecode{%s}{%s}, %s\n" % (indentation, outputname, outputcode, option))
     for subnode in node.descendants:
         tikz(subnode, out, level=level+1)
-    out.write(f"{indentation}]")
+    out.write(f"{indentation}]\n")
 
+def write_tikz(tree, filename):
+    with open(f"treetex/{filename}.tex", 'w') as out:
+        out.write(textemplate[0])
+        tikz(tree, out)
+        out.write(textemplate[1])
 
 textemplate = (r"""\documentclass{standalone}
 \usepackage[linguistics,edges]{forest}
 \usepackage{libertine}
-\forestset{
-  every leaf node/.style={
-    if n children=0{#1}{}
-  }
-}
 \input{definitions.tex}
 \begin{document}
 \tiny
 \begin{forest} for tree={grow'=east,
                            delay={where content={}{shape=coordinate}{}},
-                           every leaf node={rounded corners=2pt, draw, fill=black!5}
                            },
                forked edges,
 """,
@@ -38,12 +53,26 @@ r"""\end{forest}
 \end{document}
 """)
 
+
+
 if __name__ == "__main__":
     offset = 9999999
+    #offset = 4
     trees = read('newick_nodistance.txt')
     for tree in trees[:offset]:
         filename =  sanitized_name(tree)[1]
-        with open(f"treetex/{filename}.tex", 'w') as out:
-            out.write(textemplate[0])
-            tikz(tree, out)
-            out.write(textemplate[1])
+        print(filename)
+        if filename in ("atla1278","aust1307","indo1319", "sino1245","afro1255"):
+            for i, subtree in enumerate(tree.descendants):
+                subfilename =  f"{filename}-{i+1}"
+                print(subfilename)
+                if subfilename in ("atla1278-5","aust1307-3","indo1319-1"):
+                    for j, subsubtree in enumerate(subtree.descendants):
+                        subsubfilename =  f"{filename}-{i+1}.{j+1}"
+                        print(subsubfilename)
+                        write_tikz(subsubtree, subsubfilename)
+                else:
+                        write_tikz(subtree, subfilename)
+        else:
+            write_tikz(tree, filename)
+
--- deco/treetex/batchtex.sh
+++ deco/treetex/batchtex.sh
@@ -1,1 +1,1 @@
- for f in *[0-9]*tex; do echo $f; xelatex -halt-on-error "$f">/dev/null && echo "OK" && continue; echo "fail";  done
+for f in *[0-9]*tex; do echo $f; xelatex -halt-on-error "$f">/dev/null && echo "OK" && continue; echo "fail";  done
